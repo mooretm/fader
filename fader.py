fa5eaae7658d0e5f0b7764a8c69aba7e77f5ef3f
Travis Moore <travis_moore@starkey.com>
2022-08-19 18:19:52 -0500

    Preliminary code that produces the transition only (i.e., no start/end stable portions), and decreases level from max to zero.

--- fader.py
+++ fader.py
@@ -4,40 +4,86 @@
     Created: 19 Aug, 2022
     Last Edited: 19 Aug, 2022
 """
-
+###########
+# Imports #
+###########
 # Imports data science packages
+from turtle import title
 import numpy as np
 from matplotlib import pyplot as plt
 
 # Import sound packages
 import sounddevice as sd
+from scipy.io import wavfile
 
 # Import custom modules
 from models import Audio
-from filters import doGate
-from filters import butter_lowpass_filter
+import filters
 
-speech = Audio('CST_Speech.wav', -15)
-babble = Audio('CST_Babble_1.wav', -20)
+#########
+# BEGIN #
+#########
+# Initialize values
+# Read in audio and set SNR to +5
+speech_obj = Audio('CST_Speech.wav', -15)
+babble_obj = Audio('CST_Babble_4.wav', -20)
 
+# Set duration parameters in seconds
+stable_dur = 1
+delta_t = 0
+total_dur = stable_dur + delta_t + stable_dur
 
-stable_dur = 5
-delta_t = 10
-total_dur = stable_dur + delta_t
+# Convert seconds to samples
+stable_dur_samps = stable_dur * speech_obj.fs
+delta_t_samps = delta_t * speech_obj.fs
+total_dur_samps = (stable_dur_samps * 2) + delta_t_samps
 
+# Grab audio clips based on total_dur
+speech = speech_obj.working_audio[0:total_dur_samps]
+babble = babble_obj.working_audio[0:total_dur_samps]
+combo = speech + babble
 
-stable_dur_samps = stable_dur * speech.fs
-speech_temp = speech.working_audio[0:stable_dur_samps]
-babble_temp = babble.working_audio[0:stable_dur_samps]
-combo_temp = speech_temp + babble_temp
+# Filter audio
+combo_low = filters.butter_filt(combo, 'low', 1000, 5, speech_obj.fs)
+combo_high = filters.butter_filt(combo, 'high', 1000, 5, speech_obj.fs)
 
-combo_gated = doGate(combo_temp, stable_dur, fs=speech.fs)
+# Plot signals
+def mk_plot(low, high):
+    a1 = plt.subplot2grid((2,2),(0,0), rowspan=1, colspan=1)
+    a2 = plt.subplot2grid((2,2),(0,1), rowspan=1, colspan=1)
+    a3 = plt.subplot2grid((2,2),(1,0), rowspan=1, colspan=2)
+    a1.plot(low, c='b')
+    a1.set(title='Freqs < 1kHz', xlabel='Samples', ylabel='Amplitude')
+    a1.set_ylim((-1,1))
+    a2.plot(high, c='r')
+    a2.set(title='Freqs > 1kHz', xlabel='Samples', ylabel='Amplitude')
+    a2.set_ylim((-1,1))
+    a3.plot(low, c='b')
+    a3.plot(high, c='r')
+    a3.set(title='Recombined Signal', xlabel='Samples', ylabel='Amplitude')
+    a3.set_ylim((-1, 1))
+    plt.suptitle("Signal Filtering around 1 kHz")
+    plt.tight_layout()
+    plt.show()
 
-sd.play(combo_gated, speech.fs)
-sd.wait(stable_dur)
+#mk_plot(combo_low, combo_high)
 
+# Splice audio into start-transition-end clips
+sig_start = combo_low[0:stable_dur_samps]
+sig_trans = combo_low[stable_dur_samps:(stable_dur_samps + delta_t_samps)]
+sig_end = combo_low[(stable_dur_samps+delta_t_samps):((stable_dur_samps*2) 
+    + delta_t_samps)]
 
+# Fade out low-frequencies
+sig_gated = filters.mk_gate(combo_low, total_dur, speech_obj.fs)
 
+mk_plot(sig_gated, combo_high)
 
+# Recombine filtered signals
+sig = sig_gated + combo_high
 
+# Present audio
+#sd.play(sig, speech_obj.fs)
+#sd.wait(total_dur)
 
+wavfile.write(str(total_dur) + '.wav', speech_obj.fs, sig)
--- filters.py
+++ filters.py
@@ -9,34 +9,19 @@ from scipy import signal
 ####################
 # Filter functions #
 ####################
-def butter_lowpass(cutoff, fs, order=5):
+def butter_filt(sig, type, cutoff, order, fs=48000):
     nyq = 0.5 * fs
-    normal_cutoff = cutoff / nyq
-    b, a = signal.butter(order, normal_cutoff, btype='low', analog=False)
-    return b, a
-
-
-def butter_lowpass_filter(data, cutoff, fs, order=5):
-    b, a = butter_lowpass(cutoff, fs, order=order)
-    #y = signal.lfilter(b, a, data)
-    y = signal.filtfilt(b, a, data)
+    norm_cutoff = cutoff / nyq
+    b, a = signal.butter(order, norm_cutoff, btype=type, analog=False)
+    y = signal.filtfilt(b, a, sig)
     return y
 
 
-def butter_filt(sig, type, cutoff, order, fs=48000):
-    if type == 'low' or type == 'high':
-        nyq = 0.5 * fs
-        norm_cutoff = cutoff / nyq
-        b, a = signal.butter(order, norm_cutoff, btype=type, analog=False)
-        y = signal.filtfilt(b, a, sig)
-        return y
-
-
-def doGate(sig, rampdur=0.02, fs=48000):
+def mk_gate(sig, win_dur, fs):
     """Apply falling ramp to signal SIG, of 
         duration RAMPDUR.         
     """
-    gate =  np.cos(np.linspace(np.pi, 2*np.pi, int(fs*rampdur)))
+    gate =  np.cos(np.linspace(np.pi, 2*np.pi, int(fs*win_dur)))
     # Adjust envelope modulator to be within +/-1
     gate = gate + 1 # translate modulator values to the 0/+2 range
     gate = gate/2 # compress values within 0/+1 range
